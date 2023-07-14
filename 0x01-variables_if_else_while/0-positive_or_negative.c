b5d48078a08a6d9ce083ef910e139a82e4297bd9
sarabouri <sarabouri18@gmail.com>
2023-07-14 16:18:16 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* main -Entry point */
-/* Descriotion: 'Positive anything is better than negative nothing' */
+/* Descriotion: 'Print if a number is positive, negative or zero'  */
 /*Return: Always 0 (Success) */
 int main(void)
 {int n;
