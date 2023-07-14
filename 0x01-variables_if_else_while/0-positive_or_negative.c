9fbcef8eed1e6ac83661c75bf6721c5d30e2b316
sarabouri <sarabouri18@gmail.com>
2023-07-14 15:57:24 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,14 @@
 /* Descriotion: 'Positive anything is better than negative nothing' */
 /*Return: Always 0 (Success) */
 int main(void)
-{
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if( n>0 )
-	{
-	printf("%d is positive\n", n); }
-	if( n==0 )
-       	{printf("%d is zero\n", n); }
-	if( n<0 )
-	{printf("%d is negative\n", n); }
-
-	return (0);
+{int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if( n>0 )
+{printf("%d is positive\n", n); }
+if( n==0 )
+{printf("%d is zero\n", n); }
+if( n<0 )
+{printf("%d is negative\n", n); }
+return (0);
 }
