ca1992b45f99744589dc96d71108341ccef2980c
sarabouri <sarabouri18@gmail.com>
2023-07-13 18:43:09 -0700

    My commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,6 @@ int main(void)
 {printf("Size of a char: %zu bytes\n" , sizeof(char));
 	printf("Size of an int:%zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n" , sizeof(long));
-	printf("Size of a long long: %zu bytes\n" , sizeof(long long));
+	printf("Size of a long long int: %zu bytes\n" , sizeof(long long));
 	printf("Size of a float: %zu bytes\n" , sizeof(float);
        	return (0);}
