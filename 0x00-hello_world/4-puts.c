d0873e0a86c551ebb48688b007c606c7814e6e75
sarabouri <sarabouri18@gmail.com>
2023-07-13 17:38:24 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 #include <stdio.h>
-int main (void)
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle \n"
+    return (0);
+
+;
 	return (0)
 
 	}
