ae11f72a28d5a7c5b57c5b767595acf680f917ec
sarabouri <sarabouri18@gmail.com>
2023-07-14 14:15:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* your code goes there */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+	{
+	printf("%d is positif\n", n)}
+	else if ("%d is zero\n",n)}
+	else ("ùd is negative\n",n)}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+variables README file
