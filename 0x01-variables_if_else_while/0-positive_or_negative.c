319353677ce38d763689a6ce24c178ec445a17f0
sarabouri <sarabouri18@gmail.com>
2023-07-14 16:41:31 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** main -Entry point 
-*Descriotion:'Print if a number is positive, negative or zero'
+/**
+main -Entry point 
+*Descriotion: 'Print if a number is positive, negative or zero'
 *Return: Always 0 (Success)
 */
 int main(void)
