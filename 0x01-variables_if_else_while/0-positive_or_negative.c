d41f093c3d4a6bb450339c2e0a5703821a4fb7b3
sarabouri <sarabouri18@gmail.com>
2023-07-14 14:54:08 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,9 +12,9 @@ int main(void)
 	if(n>0)
 	{
 	printf("%d is positif\n",n ); }
-	else if(n==0)
+	if(n==0)
        	{printf("%d is zero\n",n ); }
-	else(n<0){printf("%d is negative\n",n ); }
+	if(n<0){printf("%d is negative\n",n ); }
 
 	return (0);
 }
