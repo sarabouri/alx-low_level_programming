d84586a75bb6c2e0a5ff7461203f3662a5a21d68
sarabouri <sarabouri18@gmail.com>
2023-07-13 18:38:07 -0700

    My commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{printf("Size of a char: %zu bytes\n" , sizeof(char));
+	printf("Size of an int:%zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n" , sizeof(long));
+	printf("Size of a long long: %zu bytes\n" , sizeof(long long));
+	printf("Size of a float: %zu bytes\n" , sizeof(float);
+       	return (0);}
