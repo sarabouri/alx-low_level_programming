62c544c07b015127a0a9d3352268c9849364c292
sarabouri <sarabouri18@gmail.com>
2023-07-16 20:03:16 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main -Entry point
+*Description: printing numbers from 0 to 9
+*Return: always 0 (success)
+*/
+
+int main(void)
+{int i;
+	for (i = 0 ; i < 10 ; i++)
+
+		printf("%d", i);
+
+	printf("\n");
+
+return (0); }
