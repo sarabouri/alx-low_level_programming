b12ed4f90aa8125d9af9fba01df72d1e20118d35
sarabouri <sarabouri18@gmail.com>
2023-07-16 18:25:47 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-kgjy
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: printing alphabet
+* *Return: Always 0 (success)
+*/
+int main(void)
+{char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+
+	{putchar(alp[i]); }
+		putchar('\n');
+
+	return (0);
+
+}
