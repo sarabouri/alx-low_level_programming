0c80098ee0c7f5bfbf04a734e0234bc49be403ee
sarabouri <sarabouri18@gmail.com>
2023-07-19 12:34:15 -0700

    Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void _putchar(char c) {
+    putchar(c);
+}
+
+void print_alphabet(void) {
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        _putchar(letter);
+    }
+
+    _putchar('\n');
+}
+
+int main() {
+    print_alphabet();
+    return 0;
+}
