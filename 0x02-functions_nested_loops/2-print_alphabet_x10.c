becfb9c90ec8aeb38a4d6938adb0e8833c071c22
sarabouri <sarabouri18@gmail.com>
2023-07-19 13:51:28 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print_alphabet_x10 from a to z
+ * Return: Always à (success)
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i, j;
+	for ( i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 97 ; j <= 122 ; j++)
+		_putchar(j);
+		_putchar('\n');
+	}
+
+}
