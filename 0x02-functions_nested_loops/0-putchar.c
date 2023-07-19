babfa915239c92b10dfbf575165dc92708b484d7
sarabouri <sarabouri18@gmail.com>
2023-07-19 11:12:43 -0700

    Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Decription: printing _putchar followed by new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	printf("_putchar\n");
+
+	return(0);
+}
