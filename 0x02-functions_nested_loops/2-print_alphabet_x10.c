d3c68da7f8207dffadceba430ead32bc2188deae
sarabouri <sarabouri18@gmail.com>
2023-07-19 13:56:57 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,13 @@ void print_alphabet_x10(void)
 
 {
 	int i, j;
-	for ( i = 1 ; i <= 10 ; i++)
+
+	for (i = 1 ; i <= 10 ; i++)
 	{
 		for (j = 97 ; j <= 122 ; j++)
-		_putchar(j);
+
+			_putchar(j);
+
 		_putchar('\n');
 	}
 
