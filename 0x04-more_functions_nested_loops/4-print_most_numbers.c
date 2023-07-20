6611cecd944e277350e11d034f758e8b81e2246d
sarabouri <sarabouri18@gmail.com>
2023-07-20 16:47:25 -0700

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,27 @@
 #include "main.h"
 /**
- *
  *print_numbers - function that prints the numbers, from 0 to 9
  *Description: printing numbers from 0 to 9
  *Return: Always 0 (success)
- *
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
-	for (i = 48; i <= 57 ; i++à)
+	i = 48;
+
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
+
+		{
+			i += 1;
+		}
 	{
 		_putchar(i);
+		i++;
 	}
 	_putchar('\n');
 
 }
+}
+
