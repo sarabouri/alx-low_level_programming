547167e997e7968b08d78a08fc11c06af7c8121a
sarabouri <sarabouri18@gmail.com>
2023-07-20 14:33:39 -0700

    m

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ * _isdigit - function that checks for a digit (0 through ç)
+ *
+ *@c: entry by user
+ *Return: 1 if is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+		else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 int _isupper(int c);
-
+int _isdigit(int c);
+int mul(int a, int b);
 
 
 
