68afc69cb37dc3c0a4615f0bcbbbd6d91f500aaa
sarabouri <sarabouri18@gmail.com>
2023-07-19 17:38:32 -0700

    m

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,9 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-		return (2);
-	return (1); 
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0); 
 }
