9e92089cae745bfe940a403774426b2593a72e41
sarabouri <sarabouri18@gmail.com>
2023-07-25 17:53:54 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_stlen - return the length of a string
+ *@s: input string
+ *Return: mength of a string
+ */
+int _strlen(char *s)
+{
+	int count = *0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+
+}
