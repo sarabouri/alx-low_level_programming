417a129fb651e941d0fdf61a559552a265f0836e
sarabouri <sarabouri18@gmail.com>
2023-09-29 02:29:14 +0000

    m

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+#include "main.h"
+
+int main(void)
+{	
+	int n;
+
+	n = get_endianness();
+	
+	if (n != 0)
+
+	{
+		printf("Little Endian\n");
+
+	else
+	{
+		printf("Big Endian\n");
+	}
+return (0);
+}
